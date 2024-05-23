902dea7fbe5f7a81b4f724a331f598c1356913c0
Elcapi344 <juansecuadros344@gmail.com>
2024-05-23 09:19:51 -0500

    Rutas completadas

--- app/server.js
+++ app/server.js
@@ -6,13 +6,5 @@ const app = express();
 export default app;
 
 
-// import express from "express";
-// import ruta from "./routes";
-
-// const app = express();
-
-// app.use("/", ruta);
-
-// export default app;
 
 
