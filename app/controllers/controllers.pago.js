80f6ab9a1c1f63066121197ccce5b9ebf1c98d2b
Elcapi344 <juansecuadros344@gmail.com>
2024-05-23 09:48:40 -0500

    Controladores para las rutas

---
+++ app/controllers/controllers.pago.js
@@ -0,0 +1,42 @@
+import pool from "../config/mysql.db";
+
+export const showPago = async (req, res)=>{
+    const id = req.params['id'];
+    try {
+        
+    } catch (error) {
+        
+    }    
+};
+
+export const listPago = (req, res => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+export const insertPago = (req, res => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+export const updatePago = (req, res => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+export const deletePago = (req, res => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
